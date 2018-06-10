6571be9157f53e81d117055b91433f70d94c6296
Andy Walpole <me@andywalpole.me>
2018-06-10 15:06:45 +0100

    excerise one

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-// Put this in the script section in JSFiddle
-// In a local setup, you need to merge this and the index.html file into one file
-new Vue({
+const vm = new Vue({
 	el: '#app',
 	data: {
-		title: 'Hello World!'
-	}
+		title: 'Hello World!',
+		link: 'http://example.co.uk',
+		finishLink: '<a href="http://example.co.uk">This is an example</a>'
+	},
+	methods: {
+		sayHello: function() {
+			return this.title;
+		}
+	},
+	template: `<div>
+								<h1 v-once>{{title}}</h1>
+								<p v-html="finishLink"></p>
+								<p>{{ sayHello() }} - <a v-bind:href="link">Google</a></p>
+						</div>`
 })
